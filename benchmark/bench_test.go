40ad3b052cd84c22380a6abfae96912c6f8c3035
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2019-04-25 21:56:11 +0200

    Benchmark pin toggling with and without mutex

--- benchmark/bench_test.go
+++ benchmark/bench_test.go
@@ -2,11 +2,67 @@ package benchmark
 
 import (
 	"testing"
-	"time"
+
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
+/*
 func BenchmarkTest(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		time.Sleep(time.Millisecond)
 	}
+}
+*/
+
+func BenchmarkOnOff(b *testing.B) {
+	const pin = 17
+
+	// FIXME: can't set pin mode to output in my version yet, but it persists so this hack works
+	if err := rpio.Open(); err != nil {
+		b.Fatal(err)
+	}
+	rpio.Pin(pin).Output()
+	_ = rpio.Close()
+
+	b.Run("library", func(b *testing.B) {
+		if err := rpio.Open(); err != nil {
+			b.Fatal(err)
+		}
+		defer rpio.Close()
+
+		b.ResetTimer() // in case Open() is slow
+
+		for i := 0; i < b.N; i++ {
+			rpio.Pin(pin).High()
+			rpio.Pin(pin).Low()
+		}
+	})
+	b.Run("singlethreaded-one", func(b *testing.B) {
+		g, err := newGPIO()
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer g.Close()
+
+		b.ResetTimer() // in case newGPIO() is slow
+
+		for i := 0; i < b.N; i++ {
+			g.WritePin(pin, false)
+			g.WritePin(pin, true)
+		}
+	})
+	b.Run("singlethreaded-multi", func(b *testing.B) {
+		g, err := newGPIO()
+		if err != nil {
+			b.Fatal(err)
+		}
+		defer g.Close()
+
+		b.ResetTimer() // in case newGPIO() is slow
+
+		for i := 0; i < b.N; i++ {
+			g.WritePins(1<<pin, false)
+			g.WritePins(1<<pin, true)
+		}
+	})
 }
--- benchmark/empty.go
+++
@@ -1,2 +0,0 @@
-// Package benchmark must contain a non-test file or Go gets a little annoyed.
-package benchmark
